f0c9f5dbb19946295871bfbddbf72bb28a1c8426
Eli Hause <elijhause@gmail.com>
2026-02-22 21:37:23 -0500

    Optimizing with groups

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from constants import SCREEN_WIDTH
-from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
@@ -9,26 +8,39 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    dt = 0
+    
+    #1 Create groups
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #2 Register Player class with groups
+
+    Player.containers = (updatable, drawable)
+
+    #3 Create the player
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        log_state()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        dt = clock.tick(60) / 1000
+
+        #4 Use groups
+
+        updatable.update(dt)
+
+        log_state()
+
         screen.fill("black")
-
-        player.update(dt)
-
-        player.draw(screen) # draw the triangle
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
-
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
