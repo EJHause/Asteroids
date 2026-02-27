32ff9d86a6dd9463d4baf8b5e8b728b277f92c8b
Eli Hause <elijhause@gmail.com>
2026-02-26 22:14:06 -0500

    Perfect the shot

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -35,8 +36,11 @@ class Player(CircleShape):
             self.move(dt) # move forward
         if keys[pygame.K_s]:
             self.move(-dt) # move backward
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.shot_timer <= 0:
             self.shoot() # shoot
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+        
+        self.shot_timer -= dt  
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
